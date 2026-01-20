6167ca3e82b065914bb47a3bb6d61c9506b14415
Aluru Anitha <anithaaluru99@gmail.com>
2026-01-20 06:09:57 +0000

    terraform

--- main.tf
+++ main.tf
@@ -1,7 +1,14 @@
-
 module "vpc" {
-  source = "../terraform-aws-vpc"
-  project = var.project
-  environment = var.environment
-  public_subnets_cidrs = ["10.0.1.0/24","10.0.2.0/24"]
+    #source = "../terraform-aws-vpc"
+    source = "git::https://github.com/anithaaluru/terraform-aws-vpc.git?ref=main"
+    /* project = "roboshop"
+    environment = "dev"
+    public_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"] */
+    project = var.project
+    environment = var.environment
+    public_subnet_cidrs = var.public_subnet_cidrs
+    private_subnet_cidrs = var.private_subnet_cidrs
+    database_subnet_cidrs = var.database_subnet_cidrs
+
+    is_peering_required = true
 }
